105d6e881a46c4df8908c8497f451906276291f9
jwatts <joseph.watts@peapoddigitallabs.com>
2022-01-19 23:10:42 -0600

    plantuml file list

--- src/main/sh/build-plantuml.sh
+++ src/main/sh/build-plantuml.sh
@@ -7,11 +7,10 @@ mkdir IMAGES_DIR
 for i in $(find ${SITE_DIR} -name *.puml)
 do
   echo "File: $i"
-
   REL_FILE=$(echo $i | sed -e "s|${SITE_DIR}||")
 
   echo "REL: ${REL_FILE}"
 
-  #java -jar /app/plantuml.jar -tsvg -charset utf-8 -r -o src/main/mkdocs/built-site/images -I $i
+  java -jar /app/plantuml.jar -tsvg -charset utf-8 -r -o ${IMAGES_DIR}${REL_FILE} -I $i
 
 done
