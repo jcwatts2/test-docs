1d374b21480068d20ab16916aa72169917236b8e
jwatts <joseph.watts@peapoddigitallabs.com>
2022-01-19 23:01:07 -0600

    plantuml file list

--- src/main/sh/build-plantuml.sh
+++ src/main/sh/build-plantuml.sh
@@ -2,7 +2,16 @@
 SITE_DIR="${GITHUB_WORKSPACE}/src/main/mkdocs/site"
 IMAGES_DIR="${SITE_DIR}/images"
 
+mkdir IMAGES_DIR
+
 for i in $(find ${SITE_DIR} -name *.puml)
 do
   echo "File: $i"
+
+  REL_FILE=$(echo $i | sed -e "s/${SITE_DIR}//")
+
+  echo "REL: ${REL_FILE}"
+
+  #java -jar /app/plantuml.jar -tsvg -charset utf-8 -r -o src/main/mkdocs/built-site/images -I $i
+
 done
