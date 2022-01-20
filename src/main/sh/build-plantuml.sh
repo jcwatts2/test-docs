2300face531c183aec624061967f5cfa67070858
jwatts <joseph.watts@peapoddigitallabs.com>
2022-01-20 15:23:31 -0600

    Relative Path Test

--- src/main/sh/build-plantuml.sh
+++ src/main/sh/build-plantuml.sh
@@ -1,4 +1,3 @@
-
 SITE_DIR="${GITHUB_WORKSPACE}/src/main/mkdocs/site"
 IMAGES_DIR="${SITE_DIR}/images"
 
@@ -7,9 +6,6 @@ mkdir IMAGES_DIR
 for i in $(find ${SITE_DIR} -name *.puml)
 do
   REL_DIR=$(echo $i | sed -e "s|${SITE_DIR}||" -e "s|\/[A-Za-z1-9-]\+\.puml$||")
-
   echo "Generating PlantUML ${i} to directory ${IMAGES_DIR}${REL_DIR}"
-
   java -jar /app/plantuml.jar -tsvg -charset utf-8 -r -o ${IMAGES_DIR}${REL_DIR} $i
-
 done
