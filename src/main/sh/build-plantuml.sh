b9b542c2476c0e544c709cc834e756bbd0a2b81a
jwatts <joseph.watts@peapoddigitallabs.com>
2022-01-19 23:23:21 -0600

    plantuml file list3

--- src/main/sh/build-plantuml.sh
+++ src/main/sh/build-plantuml.sh
@@ -7,7 +7,7 @@ mkdir IMAGES_DIR
 for i in $(find ${SITE_DIR} -name *.puml)
 do
   echo "File: $i"
-  REL_FILE=$(echo $i | sed -e "s|${SITE_DIR}||" | sed -e "s|/.*$||")
+  REL_FILE=$(echo $i | sed -e "s|${SITE_DIR}||" -e "s|/.*$||")
 
   echo "REL: ${REL_FILE}"
 
