17a4e77ca5902b595af18417dc0f1bf34f919f6b
jwatts <joseph.watts@peapoddigitallabs.com>
2022-01-19 22:52:26 -0600

    plantuml file list

--- src/main/sh/build-plantuml.sh
+++ src/main/sh/build-plantuml.sh
@@ -1,4 +1,8 @@
-for i in $(find ${GITHUB_WORKSPACE} -name *.puml -printf "%P")
+
+SITE_DIR="${GITHUB_WORKSPACE}/src/min/mkdocs/site"
+IMAGES_DIR="${SITE_DIR}/images"
+
+for i in $(find ${SITE_DIR} -name *.puml)
 do
   echo "File: $i"
 done
