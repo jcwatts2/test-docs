ae952cbbc985b1f6134a28bc7b642a5ebae919f8
jwatts <joseph.watts@peapoddigitallabs.com>
2022-01-20 23:55:30 -0600

    update paths3

--- src/main/sh/build-plantuml.sh
+++ src/main/sh/build-plantuml.sh
@@ -1,8 +1,9 @@
 SITE_DIR="${GITHUB_WORKSPACE}/src/main/mkdocs/site"
+DOCS_DIR="${GITHUB_WORKSPACE}/src/main/mkdocs/docs"
 
 mkdir IMAGES_DIR
 
-for i in $(find ${SITE_DIR} -name *.puml)
+for i in $(find ${DOCS_DIR} -name *.puml)
 do
   REL_DIR=$(echo $i | sed -e "s|${SITE_DIR}||" -e "s|\/[A-Za-z1-9-]\+\.puml$||")
   echo "Generating PlantUML ${i} to directory ${SITE_DIR}${REL_DIR}"
