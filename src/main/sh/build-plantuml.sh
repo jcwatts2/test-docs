58799086b27c89da68ef211a7f450321c72fbb74
jwatts <joseph.watts@peapoddigitallabs.com>
2022-01-21 01:00:28 -0600

    build order6

--- src/main/sh/build-plantuml.sh
+++ src/main/sh/build-plantuml.sh
@@ -6,6 +6,7 @@ mkdir IMAGES_DIR
 for i in $(find ${DOCS_DIR} -name *.puml)
 do
   REL_DIR=$(echo $i | sed -e "s|${DOCS_DIR}||" -e "s|\/[A-Za-z1-9-]\+\.puml$||")
-  echo "Generating PlantUML ${i} to directory ${SITE_DIR}${REL_DIR}"
-  java -jar /app/plantuml.jar -v -tsvg -charset utf-8 -r -o ${SITE_DIR} $i
+  IMG_DIR="${SITE_DIR}${REL_DIR}"
+  echo "Generating PlantUML ${i} to directory ${IMG_DIR}"
+  java -jar /app/plantuml.jar -v -tsvg -charset utf-8 -r -o ${IMG_DIR} $i
 done
