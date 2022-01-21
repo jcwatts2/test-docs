e9b6242424e8a1521f7206065d086ed4cedcd70f
jwatts <joseph.watts@peapoddigitallabs.com>
2022-01-20 23:59:35 -0600

    update paths4

--- src/main/sh/build-plantuml.sh
+++ src/main/sh/build-plantuml.sh
@@ -5,7 +5,7 @@ mkdir IMAGES_DIR
 
 for i in $(find ${DOCS_DIR} -name *.puml)
 do
-  REL_DIR=$(echo $i | sed -e "s|${SITE_DIR}||" -e "s|\/[A-Za-z1-9-]\+\.puml$||")
+  REL_DIR=$(echo $i | sed -e "s|${DOCS_DIR}||" -e "s|\/[A-Za-z1-9-]\+\.puml$||")
   echo "Generating PlantUML ${i} to directory ${SITE_DIR}${REL_DIR}"
   java -jar /app/plantuml.jar -tsvg -charset utf-8 -r -o ${SITE_DIR} $i
 done
