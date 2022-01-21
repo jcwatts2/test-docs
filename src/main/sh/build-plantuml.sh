c3502fa2907952b10a02c24043a0467593605c67
jwatts <joseph.watts@peapoddigitallabs.com>
2022-01-21 00:56:23 -0600

    build order6

--- src/main/sh/build-plantuml.sh
+++ src/main/sh/build-plantuml.sh
@@ -1,4 +1,4 @@
-SITE_DIR="${GITHUB_WORKSPACE}/src/main/mkdocs/site"
+SITE_DIR="${GITHUB_WORKSPACE}/src/main/mkdocs/docs"
 DOCS_DIR="${GITHUB_WORKSPACE}/src/main/mkdocs/docs"
 
 mkdir IMAGES_DIR
@@ -7,5 +7,5 @@ for i in $(find ${DOCS_DIR} -name *.puml)
 do
   REL_DIR=$(echo $i | sed -e "s|${DOCS_DIR}||" -e "s|\/[A-Za-z1-9-]\+\.puml$||")
   echo "Generating PlantUML ${i} to directory ${SITE_DIR}${REL_DIR}"
-  java -jar /app/plantuml.jar -tsvg -charset utf-8 -r -o ${SITE_DIR} $i
+  java -jar /app/plantuml.jar -v -tsvg -charset utf-8 -r -o ${SITE_DIR} $i
 done
